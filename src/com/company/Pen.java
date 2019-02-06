e2489ce104dfde6b1eb4bb177b416dc8fd643294
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-06 10:28:21 -0500

    In ZooDemo class create new animalPen ArrayList.

--- src/com/company/Pen.java
+++ src/com/company/Pen.java
@@ -5,4 +5,14 @@ public class Pen {
 
     private ArrayList<Animal> animals = new ArrayList<>();
     private ArrayList<BabyAnimal> babyAnimals = new ArrayList<>();
+
+    public void addAnimalToLibrary(Animal animal){
+        animals.add(animal);
+
+    }
+
+    public void addBabyAnimalToLibrary(BabyAnimal babyAnimal){
+        babyAnimals.add(babyAnimal);
+
+    }
 }
--- src/com/company/ZooDemo.java
+++ src/com/company/ZooDemo.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class ZooDemo {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<Pen> animalPen = new ArrayList<>();
     }
 }
