db8f6b50944fc85e3e7f792c480a04ca42fe39f3
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-06 10:14:59 -0500

    In Animal class, create three properties and set them as parameters in a constructor.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Animal {
+
+    private String species;
+    private String size;
+    private String gender;
+
+    public Animal(String species,String size,String gender){
+        this.species = species;
+        this.size = size;
+        this.gender = gender;
+    }
 }
