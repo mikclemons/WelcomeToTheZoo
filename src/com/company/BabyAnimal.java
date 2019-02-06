01c2ff9b6532cbac5dc8378777096be632c28f62
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-06 10:19:08 -0500

    In BabyAnimal class create a new property called canWalk. In BabyAnimal class extend to inherit from the Animal class. In BabyAnimal class create parameterized constructor using super().

--- src/com/company/BabyAnimal.java
+++ src/com/company/BabyAnimal.java
@@ -1,4 +1,16 @@
 package com.company;
 
-public class BabyAnimal {
+public class BabyAnimal extends Animal {
+
+    private boolean canWalk;
+
+
+    public BabyAnimal(String species, String size, String gender, boolean canWalk) {
+        super(species, size, gender);
+        this.canWalk = canWalk;
+        this.setSpecies(species);
+        this.setSize(size);
+        this.setGender(gender);
+
+    }
 }
