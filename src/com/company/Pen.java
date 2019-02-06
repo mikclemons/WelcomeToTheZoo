4d97a8719e4a5b745c232cb13d8066fc943555c4
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-06 10:12:09 -0500

    In Pen class create two properties of animals and babyAnimal objects.

--- src/com/company/Pen.java
+++ src/com/company/Pen.java
@@ -1,4 +1,8 @@
 package com.company;
+import java.util.ArrayList;
 
 public class Pen {
+
+    private ArrayList<Animal> animals = new ArrayList<>();
+    private ArrayList<BabyAnimal> babyAnimals = new ArrayList<>();
 }
