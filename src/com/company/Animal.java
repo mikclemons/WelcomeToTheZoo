a2b684eafc53d800b429c8c7282d474340fe434f
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-06 10:16:03 -0500

    In Animal class create accessor methods for the properties.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -11,4 +11,28 @@ public class Animal {
         this.size = size;
         this.gender = gender;
     }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
